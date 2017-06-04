8f682493a5ef2d1501b13d1826d7e4d33c9f9815
Appear <tjrwls0710@naver.com>
2017-06-04 13:24:15 +0900

    Update Exam_01.js

--- Exam/02_JavascriptControlFlow/Exam_01.js
+++ Exam/02_JavascriptControlFlow/Exam_01.js
@@ -1,3 +1,28 @@
 /**
  * Created by gilbert on 2017. 5. 31..
  */
+
+// 점수 국 수 사 과
+var result = [90,70,100,50];
+
+// 점수계산해주는 함수
+function calc(score){
+  var s = Math.floor(score/10); // ex. 100점이면 나누기 10 --> 10 A학점
+	
+  switch(s) {
+    case 조건:
+      console.log('A학점 입니다.');
+    case 조건:
+      console.log('B학점 입니다.');
+    case 조건:
+      console.log('C학점 입니다.');
+    default: 
+      console.log('D학점 입니다.');
+	}
+  
+}
+
+// 실행
+for(var i = 0; i < result.length; i++){
+	calc(result[i]);
+}
