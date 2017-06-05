3ad823b9dadaa6345d8690323d79b02ea33d3250
gilbert-go <gilbert.go@kakaocorp.com>
2017-06-05 15:54:28 +0900

    Object readme, 예제추가

--- Exam_js/03_Javascript_Object/Exam_01.js
+++ Exam_js/03_Javascript_Object/Exam_01.js
@@ -6,8 +6,9 @@
 // 3. 객체에 getName이라는 메서드를 만든다. getName은 자기 자신의 이름을 console에 찍어준다.
 // 4. name 속성의 값을 변경한다.
 // 5. 다시 getName을 이용해서 name 속성의 값을 찍어본다.
-// 6. age속성을 삭제한다.
-// 7. 최종 Object를 탐색하여 console에 찍는다.
+// 6. name속성을 삭제한다.
+// 7. 조건문을 사용해서 name속성이 없다면 다시 name 속성을 추가해준다.
+// 8. 최종 Object를 탐색하여 console에 찍는다.
 
 // 사용자 정의 생성자 함수
 // name과 age setName getName 메서드를 가지고 있는 생성자 함수를 만든다.
