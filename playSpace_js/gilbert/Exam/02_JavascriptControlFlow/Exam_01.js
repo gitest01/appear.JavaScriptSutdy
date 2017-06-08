ddeaeb951b7dd124b5d5db6613167fd1c1128ae1
gilbert-go <gilbert.go@kakaocorp.com>
2017-06-08 10:23:51 +0900

    eslint  추가

--- playSpace_js/gilbert/Exam/02_JavascriptControlFlow/Exam_01.js
+++ playSpace_js/gilbert/Exam/02_JavascriptControlFlow/Exam_01.js
@@ -10,30 +10,29 @@
  */
 
 // 점수 국 수 사 과
-var result = [40,20,86,100]; // [90,70,100,50]
+var result = [40, 20, 86, 100]; // [90,70,100,50]
 
 // 점수계산해주는 함수
-function calc(score){
-    console.log(score); // 점수
-    var resultCase = Math.floor(score/10);
+function calc(score) {
+  var resultCase = Math.floor(score / 10);
 
-    switch(resultCase) {
-        case 10:
-            console.log('A학점 입니다.');
-            break;
-        case 9:
-            console.log('B학점 입니다.');
-            break;
-        case 8:
-            console.log('C학점 입니다.');
-            break;
-        default:
-            console.log('D학점 입니다.');
-    }
+  switch (resultCase) {
+    case 10:
+      console.log('A학점 입니다.');
+      break;
+    case 9:
+      console.log('B학점 입니다.');
+      break;
+    case 8:
+      console.log('C학점 입니다.');
+      break;
+    default:
+      console.log('D학점 입니다.');
+  }
 }
 
 // 실행
-for(var i = 0; i < result.length; i++){
-    calc(result[i]);
+for (var i = 0; i < result.length; i++) {
+  calc(result[i]);
 }
 
